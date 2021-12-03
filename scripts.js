3c274ba92bb216cf75b0fe18de4b0f5be19932c0
Phat Tran <71514299+phattran01@users.noreply.github.com>
2021-12-03 08:18:01 -0500

    layout fixes

--- scripts.js
+++ scripts.js
@@ -1,69 +1,64 @@
 
 var wordChoices = MyArray;
-
-
-  var result = 0;
-  var wordStorage = '';
-  var guesses = '';
-  var missStorage = '';
-  var word = document.querySelector('#word');
-  var guess = document.querySelector('#guess');
-  var misses = document.querySelector('#misses');
-  var man = document.querySelectorAll('.man-0');
+var result = 0;
+var wordStorage = '';
+var guesses = '';
+var missStorage = '';
+var word = document.querySelector('#word');
+var guess = document.querySelector('#guess');
+var misses = document.querySelector('#misses');
+var man = document.querySelectorAll('.man-0');
   
-  function generateWordStorage() {
+function generateWordStorage() {
     wordStorage = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-  }
+}
   
-  function generateGuessWord() {
+function generateGuessWord() {
     var guessWord = '';
     var isFinish = true;
   
     for (var i = 0; i < wordStorage.length; i++) {
-      if (wordStorage[i] != ' ') {
-        if (guesses.toUpperCase().indexOf(wordStorage[i].toUpperCase()) >= 0) {
-          guessWord += wordStorage[i].toUpperCase() + '&nbsp;';
+        if (wordStorage[i] != ' ') {
+            if (guesses.toUpperCase().indexOf(wordStorage[i].toUpperCase()) >= 0) {
+            guessWord += wordStorage[i].toUpperCase() + '&nbsp;';
+            } else {
+                guessWord += '_&nbsp;';
+                isFinish = false;
+            }
+        } else {
+            guessWord += '&nbsp;&nbsp;';
         }
-        else {
-          guessWord += '_&nbsp;';
-  
-          isFinish = false;
-        }
-      }
-      else {
-        guessWord += '&nbsp;&nbsp;';
-      }
     }
   
     word.innerHTML = 'WORD: ' + guessWord;
   
     if (isFinish) {
-      result = 1;
+        result = 1;
     }
-  }
+}
   
-  function generateMisses() {
+function generateMisses() {
     var missedLetters = '';
   
     for (var i = 0; i < man.length; i++) {
-      man[i].style.display = 'none';
+        man[i].style.display = 'none';
     }
   
     for (var i = 0; i < missStorage.length; i++) {
-      document.querySelector('.man-' + (i + 1)).style.display = 'block';
+        document.querySelector('.man-' + (i + 1)).style.display = 'block';
   
-      missedLetters += missStorage[i] + ', ';
+        missedLetters += missStorage[i] + ', ';
     }
   
     missedLetters = missedLetters.substring(0, missedLetters.length - 2);
     misses.innerHTML = 'MISSES: ' + missedLetters;
   
     if (missStorage.length >= 6) {
-      result = 2;
+        result = 2;
     }
-  }
+}
   
-  function initializeGame() {
+function initializeGame() {
     result = 0;
     wordStorage = '';
     guesses = '';
@@ -78,49 +73,47 @@ var wordChoices = MyArray;
     generateMisses();
   
     guess.disabled = false;
-  }
+}
   
-  initializeGame();
+initializeGame();
   
-  guess.addEventListener('keypress', function(evt) {
+guess.addEventListener('keypress', function(evt) {
     if (evt.keyCode === 13) {
-      if (result === 0) {
-        var getTime = setInterval(function() {
-          if (wordStorage.toUpperCase().indexOf(guess.value.toUpperCase()) >= 0) {
-            guesses += guess.value.toUpperCase();
-          }
-          else {
-            if (missStorage.toUpperCase().indexOf(guess.value.toUpperCase()) < 0) {
-              missStorage += guess.value.toUpperCase();
-            }
-          }
+        if (result === 0) {
+            var getTime = setInterval(function() {
+                if (wordStorage.toUpperCase().indexOf(guess.value.toUpperCase()) >= 0) {
+                    guesses += guess.value.toUpperCase();
+                } else {
+                    if (missStorage.toUpperCase().indexOf(guess.value.toUpperCase()) < 0) {
+                    missStorage += guess.value.toUpperCase();
+                    }
+                }
   
-          guess.value = '';
+            guess.value = '';
   
-          generateGuessWord();
-          generateMisses();
+            generateGuessWord();
+            generateMisses();
   
-          clearInterval(getTime);
+            clearInterval(getTime);
   
-          var getTime2 = setInterval(function() {
-            if (result === 1) {
-              guess.disabled = true;
+            var getTime2 = setInterval(function() {
+                if (result === 1) {
+                guess.disabled = true;
   
-              if (confirm('You won! You have ' + missStorage.length + ' miss(es). ' + 'Do you like to try again?')) {
-                initializeGame();
-              }
-            }
-            else if (result === 2) {
-              guess.disabled = true;
+                if (confirm('You won! You have ' + missStorage.length + ' miss(es). ' + 'Do you like to try again?')) {
+                    initializeGame();
+                }
+                } else if (result === 2) {
+                    guess.disabled = true;
   
-              if (confirm('You lose! The answer is "' + wordStorage + '". Better luck next time. Do you like to try again?')) {
-                initializeGame();
-              }
-            }
+                    if (confirm('You lose! The answer is "' + wordStorage + '". Better luck next time. Do you like to try again?')) {
+                    initializeGame();
+                    }
+                }
   
-            clearInterval(getTime2);
-          });
+                clearInterval(getTime2);
+            });
         }, 100);
-      }
     }
-  });
+}
+});
