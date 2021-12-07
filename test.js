2c2ef90d17369bcbfe3912bdd88473761a516536
Phat Tran <phat.tran.0920@gmail.com>
2021-12-07 16:53:49 -0500

    testing fuzzyset-failed attempt #1

--- test.js
+++ test.js
@@ -1,17 +1,14 @@
-const FuzzySet = require('fuzzyset')
+import FuzzySet from 'fuzzyset.mjs'
 
+function testing() {
+                const fs = new FuzzySet();
+                var ar = ['a','aa','aaa','aah','aahed','aahing','aahs','aal','aalii','aaliis','aals','aam','aani','aardvark','aardvarks','aardwolf','aardwolves','aargh','aaron','aaronic','aaronical','aaronite','aaronitic'];
+                var x = "ah";
 
-function bruh() {
-
-    const fs = FuzzySet();
-var ar = ['a','aa','aaa','aah','aahed','aahing','aahs','aal','aalii','aaliis','aals','aam','aani','aardvark','aardvarks','aardwolf','aardwolves','aargh','aaron','aaronic','aaronical','aaronite','aaronitic'];
-var x = "ah";
-
-for ( var i = 0; i < ar.length; i++) {
-    fs.add(ar[i])
-}
-
-document.getElementById("fuck").innerHTML = (String(fs.get(x)));
-}
-
+                for ( var i = 0; i < ar.length; i++) {
+                    fs.add(ar[i])
+                }
+                result = fs.get(x);
+                document.getElementById("testP").innerHTML = result;
+            }
 
